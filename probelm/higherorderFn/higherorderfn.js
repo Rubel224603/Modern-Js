d02f6421b93988d5c0628e11d49052086d5d75c2
Rubel224603 <cserubelhosen@gmail.com>
2025-10-27 18:41:31 +0600

    added object and  heigher order function

---
+++ probelm/higherorderFn/higherorderfn.js
@@ -0,0 +1,28 @@
+//heigher order function ....
+
+function  sum(fn){
+    return fn + 3;
+}
+
+function fn(a){
+    return a;
+}
+
+// fn(6);
+let result = sum(10);
+// console.log(result);
+
+    function doSoemthing(n){
+
+        function myWork(n){
+            return 1+ n;
+        }
+        //console.log(myWork(2));
+        return myWork(5);
+    }
+   console.log(doSoemthing(n));
+
+    function n(anotherNumber){
+        return anotherNumber;
+    }
+    n(5);
