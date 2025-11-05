b4529ec19c79d8dd9200bb33dacd0100e54a19f8
Rubel224603 <cserubelhosen@gmail.com>
2025-11-05 19:34:01 +0600

    problem soultion

---
+++ probelm/countElement.js
@@ -0,0 +1,14 @@
+// Problem: Count how many times each element occurs in an array.
+// Input: [1, 2, 2, 3, 2]
+// Output: {1:1, 2:3, 3:1}
+
+const arr =  [1, 2, 2, 3, 2];
+
+// for(let i = 0; i<arr.length;i++){
+//     let check = {}
+//     if(check.1){
+
+//     }
+
+
+// }
