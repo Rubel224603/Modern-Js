c011f07240820a76076aaf6bb49cc2e28a3840ff
Rubel224603 <cserubelhosen@gmail.com>
2025-11-09 19:33:25 +0600

    binary search not completed

---
+++ problem/binarySearch.js
@@ -0,0 +1,22 @@
+//Problem Binary Search...
+//find 5 ...
+
+const binaryArr = [1,3,4,5,3,4,6,7,10];
+
+let sortedArray = binaryArr.sort((a, b) => {
+   return a-b;
+});
+console.log(sortedArray);
+
+
+function binarySearch(){
+    let lower ,higher, mid;
+    lower= sortedArray[0];
+    higher = sortedArray.length - 1
+    
+    if(){
+
+    }
+}
+
+binarySearch();
--- problem/factorial.js
+++ problem/factorial.js
@@ -2,23 +2,34 @@
 // Input: 5
 // Output: 120
 
-function fact(input){
+// function fact(input){
     
-    if(input===0){
-        console.log(input +" is's Factorial= " + 1);
-        return
-    }
-    let fact = 1;
-    for(let i = 1; i<=input;i++){
-         fact= fact*i;
+//     if(input===0){
+//         console.log(input +" is's Factorial= " + 1);
+//         return
+//     }
+//     let fact = 1;
+//     for(let i = 1; i<=input;i++){
+//          fact= fact*i;
         
-        //console.log(fact)
+//         //console.log(fact)
+//     }
+//      console.log(input +" is's Factorial = " + fact)
+// }
+// fact(0);
+// fact(2);
+// fact(1);
+// fact(3);
+// fact(4);
+// fact(5);
+
+function factorial(input) {
+    if (input > 1) {
+        return input * factorial(input - 1);
+    } else {
+        //console.log(input + " is's factorial = 1");
+        return 1; 
     }
-     console.log(input +" is's Factorial = " + fact)
 }
-fact(0);
-fact(2);
-fact(1);
-fact(3);
-fact(4);
-fact(5);
+
+console.log("Factorial =", factorial(1));
