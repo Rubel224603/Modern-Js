e52e1012372574ee5c548772893e58ec74943b6e
Rubel224603 <cserubelhosen@gmail.com>
2025-11-10 15:37:03 +0600

    find element in arrya

--- problem/binarySearch.js
+++ problem/binarySearch.js
@@ -6,27 +6,29 @@ let sortedArray = binaryArr.sort((a, b) =>{
 console.log(sortedArray); //[1,  3,  4,  5, 6, 7, 10, 12, 40]
 
 function binarySearch() {
-    let target = 40;
+    let target = 3;
     let lowerIndex = 0;
     let higherIndex = sortedArray.length - 1;
     let midIndex;
+    console.log("Terget Value = " + target+"\n");
 
     console.log("Lower Index = " + lowerIndex);
     console.log("Higher Index = " + higherIndex);
-
+    
     while (lowerIndex <= higherIndex) {  
         midIndex = Math.floor((lowerIndex + higherIndex) / 2);  
         console.log("Middle Index= " + midIndex);
         console.log("Middle Index value= " + sortedArray[midIndex]);
 
+
         if (target == sortedArray[midIndex]) {
             return midIndex; 
         } else if (target > sortedArray[midIndex]) {
             lowerIndex = midIndex + 1;
-            console.log("Lower Update Index = "+lowerIndex);
+            console.log("\nLower Update Index = "+lowerIndex);
         } else {
             higherIndex = midIndex - 1;
-            console.log(higherIndex);
+            console.log("\nheigher Index updated= "+higherIndex);
         }
     }
 
