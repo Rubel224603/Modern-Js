a1d106ea05adcfe65df3efb75f02f114ce0bd284
Rubel224603 <cserubelhosen@gmail.com>
2025-11-12 18:04:07 +0600

    count string indivisually

---
+++ problem/countOccurence.js
@@ -0,0 +1,19 @@
+// Count Occurrences
+// Input: "banana"
+// Output: { b:1, a:3, n:2 }
+const input = 'banana';
+let output = {};
+
+for(let i =0; i<input.length;i++){
+  // console.log(input[i]);
+  let key = input[i];
+  if(output[key]){
+    
+    output[key] += 1;
+    
+   }else {
+    output[key] = 1;
+   
+   }
+}
+console.log(output);
